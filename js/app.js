3e6c8d796f19acba9a6003f68ee61c6e456b2a6f
vladimirosanchez-maker <vladimirosanchez@gmail.com>
2026-06-29 16:33:54 -0500

    mejora style app index

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log("Web cargada correctamente");
+const menuBtn = document.getElementById("menuBtn");
+const mobileMenu = document.getElementById("mobileMenu");
+
+menuBtn.addEventListener("click", () => {
+
+mobileMenu.classList.toggle("active");
+
+});
+
+document.querySelectorAll(".mobile-menu a").forEach(link=>{
+
+link.addEventListener("click",()=>{
+
+mobileMenu.classList.remove("active");
+
+});
+
+});
